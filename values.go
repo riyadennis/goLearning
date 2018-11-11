39a15c7739208ff8f6babce9f44d5acc59977616
Riya Dennis <riyacv@gmail.com>
2018-11-11 18:54:56 +0000

    code school golang

--- values.go
+++ values.go
@@ -1,12 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+type jumper interface {
+	jump() string
+	getName() string
+}
+type horse struct {
+	name   string
+	weight int
+}
+type gopher struct {
+	name    string
+	age     int
+	isAdult bool
+}
+
+func (h *horse) jump() string {
+	if h.weight > 2200 {
+		return "cannot jump"
+	}
+	return "can jump"
+}
+func (g *gopher) jump() string {
+	if g.age > 18 {
+		return "can jump"
+	}
+	return "cannot jump"
+}
 func main() {
-    fmt.Println("go" + "lang")
-    fmt.Println("1 + 1 = ", 1+1)
-    fmt.Println("2 * 2 =" , 2 * 2)
-    fmt.Println(" true && false", true && false)
-    fmt.Println(" true || false ", true || false)
-    fmt.Println("!true", !true)
+	for _, g := range getList() {
+		fmt.Printf("jumping status of %s is  %s\n", g.getName(), g.jump())
+	}
+}
+func getList() []jumper {
+	phil := &gopher{"Phil", 19, true}
+	bob := &gopher{"bob", 9, false}
+	h := &horse{"barny", 234}
+	return []jumper{phil, bob, h}
+}
+func (h *horse) getName() string {
+	return h.name
+}
+
+func (g *gopher) getName() string {
+	return g.name
 }
